af8d84932b0d3643890117862351af8c5a96105c
Fredrik Göransson <fredrik.goransson@gmail.com>
2015-12-02 16:50:24 +0100

    Add GameController

---
+++ src/Backend/rsplsdeathmatchService/Controllers/GameController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Results;
+using System.Web.Http.Tracing;
+using FFCG.RSPLS.DeathMatch.Domain;
+using FFCG.RSPLSDeathMatch.Server.Models;
+using Microsoft.Azure.Mobile.Server;
+using Microsoft.Azure.Mobile.Server.Config;
+
+namespace FFCG.RSPLSDeathMatch.Server.Controllers
+{
+    [MobileAppController]
+    public class GameController : ApiController
+    {
+        private static readonly IList<FFCG.RSPLS.DeathMatch.Domain.Game> Games;
+
+        static GameController()
+        {
+            Games = new List<Game>
+            {
+                Game.StartGame("Apocoalypse!"),
+                Game.StartGame("Ein, Zwei, Combat!"),
+                Game.StartGame("War")
+            };
+        }
+
+        // GET api/games
+        [Route("api/games")]
+        public JsonResult<FFCG.RSPLS.DeathMatch.ApiModel.Game[]> GetAll()
+        {
+            MobileAppSettingsDictionary settings = this.Configuration.GetMobileAppSettingsProvider().GetMobileAppSettings();
+            ITraceWriter traceWriter = this.Configuration.Services.GetTraceWriter();
+
+            var games = Games.Select(game => game.AsApiModel()).ToArray();
+
+            traceWriter.Info($"GET Games {games.Length}");
+            return Json<FFCG.RSPLS.DeathMatch.ApiModel.Game[]>(games);
+        }
+
+        // PUT api/games
+        [HttpPut]
+        [Route("api/games")]
+        public IHttpActionResult Put([FromBody]FFCG.RSPLS.DeathMatch.ApiModel.NewGame game)
+        {
+            MobileAppSettingsDictionary settings = this.Configuration.GetMobileAppSettingsProvider().GetMobileAppSettings();
+            ITraceWriter traceWriter = this.Configuration.Services.GetTraceWriter();
+
+            var newGame = Game.StartGame(game.Name);
+            Games.Add(newGame);
+
+            traceWriter.Info($"PUT Games {game.Name}");
+
+            return CreatedAtRoute("games", new { Name = game.Name }, newGame.AsApiModel());
+        }
+
+
+        [Route("api/games/{id}")]
+        public JsonResult<FFCG.RSPLS.DeathMatch.ApiModel.Game> Get([FromUri]string id)
+        {
+            MobileAppSettingsDictionary settings = this.Configuration.GetMobileAppSettingsProvider().GetMobileAppSettings();
+            ITraceWriter traceWriter = this.Configuration.Services.GetTraceWriter();
+
+            var activeGame = Games
+                .FirstOrDefault(game => game.Name.Equals(id, StringComparison.InvariantCultureIgnoreCase))
+                .AsApiModel();
+
+            traceWriter.Info($"GET Games/{id} {activeGame}");
+            return Json(activeGame);
+        }
+
+    }
+}
---
+++ src/Backend/rsplsdeathmatchService/Models/ApiModelExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FFCG.RSPLS.DeathMatch.Domain;
+
+namespace FFCG.RSPLSDeathMatch.Server.Models
+{
+    public static class ApiModelExtensions
+    {
+        public static FFCG.RSPLS.DeathMatch.ApiModel.Game AsApiModel(this Game game)
+        {
+            return new FFCG.RSPLS.DeathMatch.ApiModel.Game()
+            {
+                Name = game.Name,
+                Round = game.GameStatus()[0]?.RoundNumber ?? 0,
+                Players = game.Players.Count,
+            };
+        }
+    }
+}
